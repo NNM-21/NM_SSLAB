35f28f73fc3ed118eaf7b7c0d7cf9e3b39fa2698
NNM-21 <nikitamishra2101@gmail.com>
2023-08-17 22:52:44 +0530

    PROGRAM 4

---
+++ Program_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(void)
+{
+    int fd1,fd5;
+    fd1 = open("MyFiles",O_RDWR );
+    fd1 = open("MyFile",O_EXCL );
+    printf("fd1=%d\n",fd1);
+    printf("fd5=%d\n",fd5);
+    return 0;
+}
